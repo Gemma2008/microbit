0d0c5ac7a776abce5163fb1d4bc68e11f29f3fee
Gemma2008 <114920831+Gemma2008@users.noreply.github.com>
2023-01-18 13:23:52 +0100

    Add files via upload

---
+++ LED arriba a abajo-main.py
@@ -0,0 +1,20 @@
+from microbit import *
+
+bola1 = Image("00000:13731:37973:13731:00000")
+bola2 = Image("00000:00000:13731:37973:13731")
+bola3 = Image("13731:00000:00000:13731:37973")
+bola4 = Image("37973:13731:00000:00000:13731")
+bola5 = Image("00000:37973:13731:00000:00000")
+
+while True:
+    display.show(bola1)
+    sleep(200)
+    display.show(bola2)
+    sleep(200)
+    display.show(bola3)
+    sleep(200)
+    display.show(bola4)
+    sleep(200)
+    display.show(bola5)
+    sleep(200)
+

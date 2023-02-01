4166746ebbd1ff4ea16732bc65ca2f72292bcfae
Gemma2008 <114920831+Gemma2008@users.noreply.github.com>
2023-02-01 13:44:45 +0100

    Add files via upload

---
+++ 1_02_23 listas-main(1).py
@@ -0,0 +1,20 @@
+# Imports go at the top
+from microbit import *
+
+while True:
+    if button_a.was_pressed():
+        i = i - 1
+        sleep(500)
+    elif button_b.was_pressed():
+        i = i + 1
+        sleep(500)
+
+    if i>len(lista)- 1:
+        i=0
+    elif i <0:
+        i = len(lista)-1
+    else:
+        display.show(i)
+        sleep(500)
+        display.show(lista[i])
+        sleep(500)
